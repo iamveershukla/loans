983b9b6c8bd4a56cf3d0484ad8a488e17fd65b03
Veer Bahadur Shukla <iamveershukla@gmail.com>
2020-03-13 14:22:32 +0200

    Update daemon.py

--- ardoq/daemon.py
+++ ardoq/daemon.py
@@ -2,41 +2,40 @@ import configparser
 import smtplib
 import sys
 
-# initialize from properties file
-config = configparser.RawConfigParser()
-config.read('daemon.properties')
+def sendEmail():
 
-# set mail parameters
-domain          = config.get('email', 'email.domain')
-port            = config.get('email', 'email.port')
-mailId          = config.get('email', 'email.mailid')
-mailPassword    = sys.argv[1]
-toMailId        = config.get('email', 'email.tomailid')
-subject         = config.get('email', 'email.subject')
-message         = config.get('email', 'email.message')
-signature       = config.get('email', 'email.signature')
+    # initialize from properties file
+    config = configparser.RawConfigParser()
+    config.read('daemon.properties')
 
-# set daemon parameters
-filename        = config.get('daemon', 'daemon.filename')
-timestampLimit  = config.get('daemon', 'daemon.timestamplimit')
+    # set mail parameters
+    domain          = config.get('email', 'email.domain')
+    port            = config.get('email', 'email.port')
+    mailId          = config.get('email', 'email.mailid')
+    mailPassword    = sys.argv[1]
+    toMailId        = config.get('email', 'email.tomailid')
+    subject         = config.get('email', 'email.subject')
+    message         = config.get('email', 'email.message')
+    signature       = config.get('email', 'email.signature')
 
-def findOldTimestamp(filename):
-    engine.say(audio)
-    engine.runAndWait()
+    # send mail
+    server = smtplib.SMTP(domain,int(port))
+    server.ehlo()
+    server.starttls()
+    server.login(mailId, mailPassword)
+    server.sendmail(mailId, toMailId, 'subject: ' + subject + '\n\n' + 'Dear Team' + '\n\n' + message + '\n\n' + 'Best Resgards,' + '\n' + signature)
 
+def getOldestTimestamp():
 
-print(domain + ' ' + port + ' ' + mailId + ' ' + toMailId + ' ' + mailPassword + ' ' + subject + ' ' + message + ' ' + signature + ' ' + filename + ' ' + timestampLimit)
+    # initialize from properties file
+    config = configparser.RawConfigParser()
+    config.read('daemon.properties')
+    
+    # set daemon parameters
+    filename        = config.get('daemon', 'daemon.filename')
+    timestampLimit  = config.get('daemon', 'daemon.timestamplimit')
 
-'''
-fadd = ''                                       # sender's email address
-tadd = ''                                       # receiver's email address
-msg = 'Mail sent through Python!'               # Message to be sent!
-username = ''                                   # Your username(email ID)
-password = ''                                   # Your password for above email ID
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.ehlo()
-server.starttls()
-server.login(username,password)
-server.sendmail(fadd,tadd,msg)
+    print(filename + ' ' + timestampLimit)
 
-'''
+# sendEmail()
+getOldestTimestamp()
